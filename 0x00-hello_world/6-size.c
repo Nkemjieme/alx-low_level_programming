c9a0492f354fbd2bd58f0b1cd0aa06576546bc35
Nkemjieme <nkemjiemej@gmail.com>
2022-09-09 10:01:11 -0700

    new c task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main-entry point
+ * 
+ * Return value 0 (success)
+ */
+int main(void)
+{ char a:
+	int b:
+	long int c:
+	long long int d:
+	float e:
+
+	printf("Size of char: %lu byte(s)".(unsigned long)(sizeof(a));
+	printf("Size of int: %lu byte(s)".(unsigned long)(sizeof(b));
+	pintf("Size of long int: %lu byte(s)".(unsigned long)(sizeof(c);
+	printf("Size of long long int: %lu bytes(s)".(unsigned long)(sizeof(d));
+	printf("Size of float: %lu byte(s)".(unsigned long)(sizeof(e));
+	return (0)
+}
