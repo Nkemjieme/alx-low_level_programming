b41c00afead46802b0bd235be2732503a3fe42ed
Nkemjieme <nkemjiemej@gmail.com>
2022-09-09 08:42:31 -0700

    New Task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#!/bin/bash
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Retun: Always 0 (success)
+ */
+
+int main()
+{	
+	char joel[52] = "\"Programming is like building a multilingual puzzle";
+
+	puts(joel);
+	return 0;
+}
